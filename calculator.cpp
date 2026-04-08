ef38716f4e6329321943479b51f69d1694484708
Dev <dev613006644@gmail.com>
2026-04-08 19:30:55 +0530

    My first simple project on this CLI

---
+++ calculator.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fast  ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+
+// data: 8-4-2026
+// Written by: Dev patel
+// there will be no updation on this basic CLI calculator
+int main(){
+    fast;
+    // here i implimented a simple calculator that can perform basic arithmetifc operations
+
+    while(true){
+        // CLI input 
+        string cmd;
+        cin >> cmd;
+        if(cmd == "exit") break;
+        double number1, number2;
+        // here the command for addition
+        if(cmd == "add"){
+            cin >> number1 >> number2;
+            cout << number1 + number2 << "\n";
+        }
+        // here the command for subtraction
+        else if(cmd == "subtract"){
+            cin >> number1 >> number2;
+            cout << number1 - number2 << "\n";
+        }
+        // here the command for multiplication
+        else if(cmd == "multiply"){
+            cin >> number1 >> number2;
+            cout << number1 * number2 << "\n";
+        }
+        // here the command for division
+        else if(cmd == "divide"){
+            cin >> number1 >> number2;
+            if(number2 == 0){
+                cout << "Error: Division by zero" << "\n";
+            } else {
+                cout << number1 / number2 << "\n";
+            }
+        }
+        // if the user enters invalid command 
+        // we will give the message of invalid command
+        else{
+            cout << "Invalid command" << "\n";
+        }
+    }
+    return 0;
+}
